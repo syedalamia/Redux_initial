c9697aa156e1ad448038c186a2ce4ae7df1f03c5
lamianoorjahan <62371017+lamianoorjahan@users.noreply.github.com>
2021-03-25 13:36:14 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { createStore } from "redux";
+//reducer must have 2 params
+//reducer has 2 params state and action
+
+const reducer = (state = {}, action) => {
+  if (action.type === "A") {
+    return { ...state, A: "i am 'A'" };
+  }
+  if (action.type === "B") {
+    return { ...state, B: "i am B" };
+  }
+
+  return { ...state };
+};
+//creating store
+const store = createStore(reducer);
+
+store.subscribe(() => {
+  console.log("print 1", store.getState());
+});
+
+store.subscribe(() => {
+  console.log("print 2", store.getState().A);
+});
+
+//without dispatching we'll not be able to print anything
+//how many time it'll dispatch those number of time subscriber
+//will got notified also able to print
+store.dispatch({ type: "A" });
+store.dispatch({ type: "something" });
+store.dispatch({ type: "B" });
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
